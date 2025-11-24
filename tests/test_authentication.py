410b7c5557b9ec22741e8d40a0be9908003b2a77
qagorodnev123-prog <qagorodnev123@gmail.com>
2025-11-24 09:45:37 +0300

    Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,9 +1,7 @@
 from http import HTTPStatus
 import pytest
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client, PublicUsersClient
-from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
-from clients.users.users_schema import CreateUserRequestSchema
+from clients.authentication.authentication_client import AuthenticationClient
 from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
@@ -12,14 +10,11 @@ from tools.assertions.schema import validate_json_schema
 @pytest.mark.regression
 @pytest.mark.authentication
 def test_login(
-        function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        function_user: UserFixture,
         authentication_client: AuthenticationClient
 ):
-    # Запрос на логин (login_request -> request)
     request = LoginRequestSchema(email=function_user.email, password=function_user.password)
-    # Выполняем логин (login_response -> response)
     response = authentication_client.login_api(request)
-    # Валидация ответа (login_response_data -> response_data)
     response_data = LoginResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
