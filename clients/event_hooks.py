042ccdc6592fb6ad6d7307f38a56d72b5a74be92
qagorodnev123-prog <qagorodnev123@gmail.com>
2026-01-26 13:55:56 +0300

    Добавляем логирование для API автотестов. Добавил док-стринг

--- clients/event_hooks.py
+++ clients/event_hooks.py
@@ -18,9 +18,19 @@ def curl_event_hook(request: Request):
     allure.attach(curl_command, "cURL command", allure.attachment_type.TEXT)
 
 def log_request_event_hook(request: Request):
+    """
+    Логирует информацию об отправленном HTTP-запросе.
+
+    :param request: Объект запроса HTTPX.
+    """
     logger.info(f"Make {request.method} request to {request.url}")
 
 def log_response_event_hook(response: Response):
+    """
+    Логирует информацию о полученном HTTP-ответе.
+
+    :param response: Объект ответа HTTPX.
+    """
     logger.info(
         f"Got response {response.status_code} {response.reason_phrase} from {response.url}"
     )
