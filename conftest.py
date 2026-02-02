aac94efe271d23c47e3a0a3e8dba41a23dd1e3f1
qagorodnev123-prog <qagorodnev123-prog@users.noreply.github.com>
2026-02-02 09:55:05 +0000

    deploy: 0ca66cb6f35525ee292dec2254018008cd0e688e

--- conftest.py
+++
@@ -1,6 +0,0 @@
-pytest_plugins = ("fixtures.users",
-                  "fixtures.authentication",
-                  "fixtures.files",
-                  "fixtures.courses",
-                  "fixtures.exercises",
-                  "fixtures.allure")
--- fixtures/authentication.py
+++
@@ -1,7 +0,0 @@
-from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
-import pytest
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
